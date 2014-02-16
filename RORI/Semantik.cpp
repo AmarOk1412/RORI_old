c510e6f91ae8fb1d4892b78e7bbc444072db7027
Sébastien Blin <amarok@enconn.fr>
2014-02-16 23:21:29 +0100

    active & classic path

--- RORI/Semantik.cpp
+++ RORI/Semantik.cpp
@@ -2,7 +2,12 @@
 
 Semantik::Semantik() : QObject()
 {
-
+    classicPaths.append("/Regex/LDetermine/5");
+    classicPaths.append("/Regex/LDetermine/10");
+    classicPaths.append("/Regex/LDetermine/15");
+    classicPaths.append("/Regex/LDetermine/20");
+    classicPaths.append("/Regex/LDetermine/+");
+    classicPaths.append("/Regex/LIndetermine");
 }
 
 /**
@@ -12,6 +17,36 @@ Semantik::Semantik() : QObject()
 void Semantik::saySomethingSlot(QString message)
 {
     emit saySomething(message);
+}
+
+/**
+ * @brief Semantik::cutSentence
+ * @param sentences
+ * @return a QStringList with all sentence in sentences
+ */
+QStringList Semantik::cutSentence(QString sentences)
+{
+    QStringList toReturn;
+    QString temp;
+    QString separator = ",;.!?-_";
+
+    for(int i = 0; i < sentences.length(); ++i)
+    {
+        if(i < sentences.length() && separator.indexOf(sentences[i]) == -1)
+        {
+            temp += sentences[i];
+            if(i == sentences.length() - 1)
+                toReturn.append(temp);
+        }
+        else
+        {
+            if(!temp.isEmpty())
+                toReturn.append(temp);
+            temp.clear();
+        }
+    }
+
+    return toReturn;
 }
 
 /**
@@ -43,11 +78,28 @@ QStringList Semantik::getWords(QString sentence)
 
     return toReturn;
 }
+
+/**
+ * @brief Semantik::addNewFilePath a path in the arbo to treat
+ * @param newPath
+ */
+void Semantik::addNewFilePath(QString newPath)
+{
+    activePaths.append(newPath);
+}
+
+//TODO : del path into activPaths
+void Semantik::delFilePath(QString path)
+{
+    Q_UNUSED(path)
+    //Not implemented Yet
+}
+
 /**
  * @brief Semantik::workData treat the message
  * @param message
  */
 void Semantik::workData(QString message)
 {
-
+    Q_UNUSED(message);
 }
--- RORI/Semantik.h
+++ RORI/Semantik.h
@@ -11,13 +11,20 @@ class Semantik : public QObject
 public:
     Semantik();
     void workData(QString message);
+    QStringList cutSentence(QString sentences);
     QStringList getWords(QString sentence);
+    void addNewFilePath(QString newPath);
+    void delFilePath(QString path);
 
 signals:
     void saySomething(QString toSay);
 
 private slots:
     void saySomethingSlot(QString toSay);
+
+private:
+    QStringList activePaths;
+    QStringList classicPaths;
 };
 
 #endif // SEMANTIK_H
