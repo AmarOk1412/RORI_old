ef0c6e342bbaa4fe24febb056d775fc483a9eb0b
Sébastien Blin <amarok@enconn.fr>
2014-02-24 13:07:12 +0100

    Toolbar

---
+++ client Desktop/Toolbar.h
@@ -0,0 +1,50 @@
+#ifndef TOOLBAR_H
+#define TOOLBAR_H
+
+#include <QWidget>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QPushButton>
+
+/**
+ * @brief The Toolbar class for the toolbar at the left of RORI
+ */
+class Toolbar : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Toolbar(QWidget *parent = 0);
+    void setupUi();
+    void setupConnect();
+
+public slots:
+    void buttonBgColor(int button);
+    void roriModeClicked();
+    void agendaModeClicked();
+    void optionModeClicked();
+    void setColor1(QString color);
+    void setColor2(QString color);
+
+signals:
+    void roriModeClickedS();
+    void agendaModeClickedS();
+    void optionModeClickedS();
+
+protected:
+    void resizeEvent(QResizeEvent *);
+
+private:
+    QLabel *background;
+    QVBoxLayout *principalLayout;
+    QLabel *logo;
+
+    QPushButton *roriMode;
+    QPushButton *agendaMode;
+    QPushButton *optionMode;
+
+    QString color1;
+    QString color2;
+};
+
+#endif // TOOLBAR_H
