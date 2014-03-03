516c2d5154a83a6561c3b063b89767e6360509a7
Sébastien Blin <amarok@enconn.fr>
2014-03-03 01:32:00 +0100

    black screen function

--- RORICompile/Regex/LDetermine/5/basic.f
+++ RORICompile/Regex/LDetermine/5/basic.f
@@ -2,3 +2,5 @@
 function/repliques/sayHello
 (au(| |-)revoir|(à|a) la prochaine|bonne soir(ée|ee|good( |-)bye|bye)|j.y.vais)
 function/repliques/sayGoodbye
+(é|e)cran.noir|black.screen|sleep|dor(s|t)
+function/repliques/blackScreen
---
+++ RORICompile/function/repliques/blackScreen.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2
+import sys
+import time
+import random
+import subprocess
+sys.path.append('function/utils')
+import randUtils
+import endFileUtils
+
+endFileUtils.writeEndFile(randUtils.getRandomSentence('function/sentences/veille.sen'), -10, -10, 0, 0, 0, -10, -10, -10, 0, 0, -10, 0, 0, 1)
+if random.randint(0,100)%3 == 0:
+    subprocess.call('mplayer -really-quiet -msglevel all=-1 function/'+ randUtils.getRandomSound('function/sentences/veille.sound') +' -af extrastereo=0 &', shell=True)
+
+subprocess.call("xset dpms force standby", shell = True)
