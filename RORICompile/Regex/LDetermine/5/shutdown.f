a07ef0b8d1632de89db7ec0c49ad0da4769c8dd9
Sébastien Blin <amarok@enconn.fr>
2014-03-06 17:08:28 +0100

    shutdown module

---
+++ RORICompile/Regex/LDetermine/5/shutdown.f
@@ -0,0 +1,6 @@
+((E|é|e)tein(d|t)s? (le (PC|pc)|l'ordi|toi))
+function/repliques/shutdown
+((S|s)hut(down)(.{0,5}| (the computer|you)))
+function/repliques/shutdown
+((T|t)urn (the computer )off)
+function/repliques/shutdown
---
+++ RORICompile/function/repliques/shutdown.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+import sys
+import time
+import random
+import subprocess
+sys.path.append('function/utils')
+import randUtils
+import endFileUtils
+
+endFileUtils.writeEndFile(randUtils.getRandomSentence('function/sentences/shutdown.sen'), -10, -10, 0, 0, 0, -10, -10, -10, 0, 0, -10, 0, 0, 1)
+if random.randint(0,100)%3 == 0:
+    subprocess.call('mplayer -really-quiet -msglevel all=-1 function/'+ randUtils.getRandomSound('function/sentences/shutdown.sound') +' -af extrastereo=0 &', shell=True)
+
+time.sleep(5)
+subprocess.call("shutdown now", shell = True)
